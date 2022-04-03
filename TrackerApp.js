ca658030e9c1fb199e2465298e7a2965928204ee
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-04-03 09:49:30 +0800

    implemented the delete and new entry into effect

--- TrackerApp.js
+++ TrackerApp.js
@@ -2,10 +2,10 @@ class TrackerApp {
     constructor(selectorString) {
         this.mainBody = document.querySelector(selectorString);
         this.mainBody.innerHTML = TrackerApp.html();
-        this.mainBody.querySelector(".btn-sm").addEventListener("click", ()=>{
+        this.mainBody.querySelector(".btn-sm").addEventListener("click", ()=> {
         this.onClickNewEntry();
-
-        });
+        })
+       this.loadData();
 
     }
     static html(){
@@ -36,21 +36,71 @@ class TrackerApp {
     }
 static tContent(){
    return `
-        <tr>
-            <td><input class="rounded border-0 py-1" type="date" name="" id=""></td>
-            <td><input class="rounded border-0 py-1" type="text" name="" id="" placeholder="Eg. Rent, Purchase, income"></td>
-            <td><select class="rounded border-0 p-1" name="" id=""><option value="Income">Income</option><option value="Expenses">Expenses</option></select></td>
-            <td><input class="rounded border-0 py-1" type="number" name="" id=""></td>
-            <td><button class="border-0 btn btn-delete">&#10005;</button></td>
+        <tr class="new-entry-row">
+            <td><input class="rounded border-0 py-1" type="date" name="" id="date"></td>
+            <td><input class="rounded border-0 py-1" type="text" name="" id="description" placeholder="Eg. Rent, Purchase, income"></td>
+            <td><select class="rounded border-0 p-1" name="" id="type"><option value="Income">Income</option><option value="Expenses">Expenses</option></select></td>
+            <td><input class="rounded border-0 py-1" type="number" name="" id="amount"></td>
+            <td><button class="border-0 btn rounded btn-delete">&#10005;</button></td>
         </tr>
    `;
 }
 
-onClickNewEntry(){
-    
-     this.mainBody.querySelector(".new-entry").insertAdjacentHTML("beforeend", TrackerApp.tContent());
+loadData(){
+    const detailsEntries =JSON.parse(localStorage.getItem("budget-details") || "[]");
+    for(const entry of detailsEntries){
+        this.tRowEntries(entry)
+    }
+}     
+
+rowEntries(){
+    return Array.from(this.mainBody.querySelectorAll(".new-entry-row"));
 }
 
+
+save(){
+      const details = this.rowEntries().map(rowData => {
+          return {
+              date: rowData.querySelector("#date").value,
+              description: rowData.querySelector("#description").value,
+              type: rowData.querySelector("#type").value,
+               amount: rowData.querySelector("#amount").value,
+               
+          };
+
+      })
+   localStorage.setItem("budget-details", JSON.stringify(details))
+
+}
+
+tRowEntries(entry = {}){
+
+    this.mainBody.querySelector(".new-entry").insertAdjacentHTML("beforeend", TrackerApp.tContent());
+     const tRow = this.mainBody.querySelector(".new-entry tr:last-of-type")
+       
+      tRow.querySelector("#date").value = entry.date || new Date().toISOString().replace(/T.*/, "");
+      tRow.querySelector("#description").value = entry.description || "";
+      tRow.querySelector("#type").value = entry.type || "Income";
+      tRow.querySelector("#amount").value = entry.amount || 0;
+      tRow.querySelector(".btn-delete").addEventListener("click", e => {
+      this.onDeletebtn(e);
+      });
+      
+      tRow.querySelectorAll(".rounded").forEach(input => {
+       input.addEventListener("change", this.save());
+      }); 
+
+}
+
+onClickNewEntry(){
+      this.tRowEntries()
+}
+
+onDeletebtn(e){
+    
+  e.target.closest("tr").remove();
+
+}
 
 
 }
